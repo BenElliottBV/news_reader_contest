598b2609644392229713b8e71bf0bce0afacf8ba
Ben Elliott <ben.elliott@bevalued.co.uk>
2016-03-18 22:27:20 +0000

    Update app.js

--- app/app.js
+++ app/app.js
@@ -9,6 +9,6 @@ application.resources = {
 }
 
 console.log("App Started!");
-appSettings.setString("token","AyGEUqiL_Dr0_lK_HoikKiDYxz-IwwLxcq-lhvkQSZkiCux9kygfkLENtdhR3Ge1UQQKOw1e-PtLCNtLlQJuKUZ9dg4aKpizS2G8_gTfvWObT5eZezQLHsOhSibO1WLr1fk7ozoIM81lcyjNxjmrd79Li92jFyt1k9vZiuM0DllIJX6j-nUq7P3YhDpPkWr3NIjhEcj-MLi7QLRx1kD7L5IDztuL5rg:feedlydev");
+appSettings.setString("token","YOURAPIKEYHERE");
 console.log(appSettings.getString("token"));
 application.start({ moduleName: "main-page" });
